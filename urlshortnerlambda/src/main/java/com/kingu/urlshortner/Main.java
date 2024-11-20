18a4775c4cdf6e9e66a67013f9c15cde3169a0f7
kingu <kingzinhu@hotmail.com>
2024-11-20 14:13:53 -0300

    Add comments

--- urlshortnerlambda/src/main/java/com/kingu/urlshortner/Main.java
+++ urlshortnerlambda/src/main/java/com/kingu/urlshortner/Main.java
@@ -10,24 +10,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
+    // JSON parser
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Override
+    @Override // AWS Lambda default function
     public Map<String, String> handleRequest(Map<String, Object> i, Context cntxt) {
+        
+        // JSON String input
         String body = (String) i.get("body");
         
+        // JSON -> Map
         Map<String, String> bodyMap;
+        
+        // Maybe the input isn't "parsable"
         try {
             bodyMap = objectMapper.readValue(body, Map.class);
         } catch (Exception exception) {
             throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
         }
 
+        // Get input URL (the long one) and it's expiration time
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
 
+        // Generate a random UUID String for the original URL
         String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
 
+        // Output
         Map<String, String> response = new HashMap<>();
         response.put("code", shortUrlCode);
     
